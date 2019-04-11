022f3b3b8393afd26f6cdf03d9e434f625d2d375
evyatar18 <evyatar38@gmail.com>
2019-04-11 15:06:57 +0300

    +FlightClient

---
+++ Ex2/Model/Client/FlightClient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2.Model.Client
+{
+    class FlightClient : IFlightClient
+    {
+        public string IP { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Port { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public bool IsOpen => throw new NotImplementedException();
+
+        public void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Open()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendLine(string line)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendLines(IList<string> lines)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
