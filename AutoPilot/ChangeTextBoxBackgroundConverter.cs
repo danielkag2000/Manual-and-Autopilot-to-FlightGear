9c2040361838619cbc0ada9ec9a8a298d1cfe1ea
danielkag2000 <45756887+danielkag2000@users.noreply.github.com>
2019-04-11 14:18:47 +0300

    update

---
+++ AutoPilot/ChangeTextBoxBackgroundConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace AutoPilot
+{
+    class ChangeTextBoxBackgroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(Brush))
+                throw new InvalidOperationException("Must convert to a brush!");
+
+            string text = (string)value;
+            return string.IsNullOrEmpty(text) ? Brushes.White : Brushes.LightPink;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
