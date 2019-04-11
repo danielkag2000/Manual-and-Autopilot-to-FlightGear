34f0a64f9d96013e5b6cd6692b85e16fd2672756
danielkag2000 <45756887+danielkag2000@users.noreply.github.com>
2019-04-11 16:17:40 +0300

    update files

---
+++ Ex2/ViewModels/AutoPilotVM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Ex2.Model.Client;
+using System.Threading.Tasks;
+
+namespace Ex2.ViewModels
+{
+    class AutoPilotVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private IFlightClient Model;
+
+        public AutoPilotVM(IFlightClient model)
+        {
+            this.Model = model;
+        }
+
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
