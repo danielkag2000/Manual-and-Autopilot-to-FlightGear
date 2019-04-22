83f661654cdb819d3dc31c6b37e41e63a8467742
danielkag2000 <45756887+danielkag2000@users.noreply.github.com>
2019-04-22 11:44:37 +0300

    add clear command

--- Ex2/ViewModels/AutoPilotVM.cs
+++ Ex2/ViewModels/AutoPilotVM.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using Ex2.Model.Client;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Ex2.ViewModels
 {
@@ -32,6 +33,19 @@ namespace Ex2.ViewModels
         public void NotifyPropertyChanged(string propName)
         {
             this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+        private ICommand _clearCommand;        public ICommand ClearCommand
+        {
+            get
+            {
+                return _clearCommand ?? (_clearCommand = new CommandHandler(() => OnClearClick()));
+            }
+        }
+
+        private void OnClearClick()
+        {
+            TextCommand = "";
         }
     }
 }
