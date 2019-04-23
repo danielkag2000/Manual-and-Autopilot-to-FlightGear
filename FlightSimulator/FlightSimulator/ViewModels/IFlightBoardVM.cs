c3eccf3a88f84134721e23c55991486dfb49a03a
evyatar18 <evyatar38@gmail.com>
2019-04-23 11:24:11 +0300

    Prepared FlightBoard for VM

---
+++ FlightSimulator/FlightSimulator/ViewModels/IFlightBoardVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulator.ViewModels
+{
+    public interface IFlightBoardVM : INotifyPropertyChanged
+    {
+        double Lon { get; }
+        double Lat { get; }
+    }
+}
